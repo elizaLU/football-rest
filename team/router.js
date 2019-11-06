0b4639bf65b31d66bc0e96954586325bae61c606
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-11-06 15:15:09 +0100

    put request for teams

--- team/router.js
+++ team/router.js
@@ -8,7 +8,8 @@ router.get('/teams', (req, res, next) => {
     .then(team => res.json(team))
     .catch(next)
 })
-
+//local: http POST :4000/teams name=eggyolk  
+//HEROKU: http POST https://fast-caverns-31315.herokuapp.com/teams name='cats and dogs'
 router.post('/teams', (req, res, next) => {
   Team.create(req.body)
     .then(team => res.json(team))
@@ -28,5 +29,18 @@ router.get('/teams/:teamId', (req, res, next) => {
     })
     .catch(next)
 })
+
+
+.put('/teams/:teamId', (req, res) => {
+  Team.findByPk(req.params.teamId)
+    .then(team => {
+      if (!team) { res.status(404).json({ message: "Team not found." }).end()}
+      else return team.update(req.body)
+    })
+    .then(updatedTeam => {
+      res.status(200).send(updatedTeam)
+    })
+})
+
 
 module.exports = router;
