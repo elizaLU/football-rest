a07dfe9caaa81c7ba34676ed929c3e227ec3271f
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-11-12 11:18:50 +0100

    signup endpoint fixed

--- user/router.js
+++ user/router.js
@@ -1,17 +1,26 @@
 const { Router } = require('express')
-const User = require('./model')
 const router = new Router()
-
 const bcrypt = require('bcryptjs')
+const User = require('./model')
 
 router.post('/signup', (req, res, next) => {
+  const name = req.body.name
+  const password = req.body.password
+  if (!name || !password) {
+    res.status(400).send({
+      message: 'Please supply a valid name and password'
+    })
+  }
   const user = {
     name: req.body.name,
     password: bcrypt.hashSync(req.body.password, 10)
   }
   User
     .create(user)
-    .then(user => res.send({ message: "Account created" }))
+    .then(user => {
+      res.send({ message: "Account created" })
+      req.user = user
+    })
     .catch(next)
 })
 
