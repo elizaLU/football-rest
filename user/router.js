87bfa2d9f4690eebea9f10ef8c3918045c8e6f60
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-11-12 14:32:21 +0100

    signup endpoint

--- user/router.js
+++ user/router.js
@@ -4,22 +4,23 @@ const bcrypt = require('bcryptjs')
 const User = require('./model')
 
 router.post('/signup', (req, res, next) => {
-  const name = req.body.name
-  const password = req.body.password
+  const { name, password } = req.body;
+  console.log("name & pass ", req.body);
+
   if (!name || !password) {
     res.status(400).send({
       message: 'Please supply a valid name and password'
     })
   }
   const user = {
-    name: req.body.name,
+    name,
     password: bcrypt.hashSync(req.body.password, 10)
   }
   User
     .create(user)
     .then(user => {
-      res.send({ message: "Account created" })
       req.user = user
+      res.send({ message: "Account created" })
     })
     .catch(next)
 })
